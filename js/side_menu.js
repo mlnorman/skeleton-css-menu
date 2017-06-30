88736d4bba693d431e5a624e06d79990e823de9b
Mike Norman <michael@mlnorman.com>
2017-06-30 17:07:38 -0400

    open button now closes also

--- js/side_menu.js
+++ js/side_menu.js
@@ -1,7 +1,16 @@
 
+    var openOrClosed = 0;
     function openNav() {
-    document.getElementById("mySidenav").style.width = "15rem";
-    document.getElementById("main").style.marginLeft = "15rem";
+    if (openOrClosed === 0) {
+        openOrClosed = 1;
+        document.getElementById("mySidenav").style.width = "15rem";
+        document.getElementById("main").style.marginLeft = "15rem";
+    }else {
+        openOrClosed = 0;
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }
+
     }
 
     function closeNav() {

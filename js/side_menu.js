5c9ce960bfad19171dca9ba1bb487dd50f1a3179
Mike Norman <michael@mlnorman.com>
2017-06-30 16:29:39 -0400

    initial

---
+++ js/side_menu.js
@@ -0,0 +1,10 @@
+
+    function openNav() {
+    document.getElementById("mySidenav").style.width = "15rem";
+    document.getElementById("main").style.marginLeft = "15rem";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+    }   
